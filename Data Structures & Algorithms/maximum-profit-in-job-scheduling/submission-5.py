da14f30f786ba7cdf860f14808c70d9143d32dec
Bill Pham <bhpham@users.noreply.github.com>
2026-09-21 18:02:25 -0500

    Add: maximum-profit-in-job-scheduling - submission-5

---
+++ Data Structures & Algorithms/maximum-profit-in-job-scheduling/submission-5.py
@@ -0,0 +1,52 @@
+'''
+Questions:
+1. are all inputs sorted?
+2. are we given only integers?
+3. is output expected to be integer?
+
+My straight forward approach is using the backtracking, memoization to cache the result, TC: O(n^2)
+We can optimize with Binary search: TC: O(n * log(n)), SC: O(n)
+'''
+
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        intervals = sorted(zip(startTime, endTime, profit))
+        n = len(intervals)
+        cache = {}
+
+        def next(l, end):
+            r = n
+            while l < r:
+                mid = (l + r) // 2
+                if intervals[mid][0] < end:
+                    l = mid + 1
+                else:
+                    r = mid
+            return l
+
+        def dfs(i):
+            if i == len(intervals):
+                return 0
+            if i in cache:
+                return cache[i]
+        
+            # dont include 
+            res = dfs(i + 1)
+
+            # include 
+            cache[i] = res = max(res, intervals[i][2] + dfs(next(i + 1, intervals[i][1])))      
+            return res
+        
+        return dfs(0)
+
+
+
+
+
+
+
+
+
+
+
+
